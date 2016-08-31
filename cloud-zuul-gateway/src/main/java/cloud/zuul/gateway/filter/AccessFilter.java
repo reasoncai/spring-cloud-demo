ddcf4c7204bae11517ff3be78729091e7f42d90d
reasoncai <403411876@qq.com>
2016-08-31 16:37:33 +0800

    zuul

---
+++ cloud-zuul-gateway/src/main/java/cloud/zuul/gateway/filter/AccessFilter.java
@@ -0,0 +1,41 @@
+package cloud.zuul.gateway.filter;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+@Component
+public class AccessFilter extends ZuulFilter{
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+
+
+        Object accessToken = request.getParameter("accessToken");
+        if(accessToken == null) {
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            return null;
+        }
+        return null;
+    }
+}
