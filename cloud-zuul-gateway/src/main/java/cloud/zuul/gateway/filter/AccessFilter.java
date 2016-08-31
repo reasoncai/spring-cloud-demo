fd1c15414be55cd8a01817ddd0e1c2327f90132a
reasoncai <403411876@qq.com>
2016-08-31 16:51:43 +0800

    打印过滤的URI，根据不同URI过滤

--- cloud-zuul-gateway/src/main/java/cloud/zuul/gateway/filter/AccessFilter.java
+++ cloud-zuul-gateway/src/main/java/cloud/zuul/gateway/filter/AccessFilter.java
@@ -27,9 +27,13 @@ public class AccessFilter extends ZuulFilter{
     @Override
     public Object run() {
         RequestContext ctx = RequestContext.getCurrentContext();
+    
         HttpServletRequest request = ctx.getRequest();
-
-
+        String requestURI = request.getRequestURI();
+        System.out.println("requestURI"+requestURI);
+        StringBuffer requestURL = request.getRequestURL();
+        System.out.println("requestURL"+requestURL.toString());
+        
         Object accessToken = request.getParameter("accessToken");
         if(accessToken == null) {
             ctx.setSendZuulResponse(false);
